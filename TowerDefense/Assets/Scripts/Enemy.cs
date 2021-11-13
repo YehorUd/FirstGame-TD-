b7e01a0faade490db945398d2a847d7e5145390c
Yehor <fifaha2017@gmail.com>
2021-11-13 15:37:44 +0100

    Init project commit
    Without waves and smooth rotations.

---
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Speed;
+    public float RotationSpeed;
+    public Transform[] Points;
+    private Animator MyAnimator;
+    private Transform _currentPoint;
+    private int _index;
+    private Vector3 _dirrection;
+    void Start()
+    {
+        _index = 0;
+        _currentPoint = Points[_index];
+        MyAnimator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        _dirrection = _currentPoint.position - transform.position;
+        Vector3 newDirrection = Vector3.RotateTowards(transform.forward, _dirrection, Time.deltaTime * RotationSpeed, 0);
+
+        transform.rotation = Quaternion.LookRotation(newDirrection);
+        transform.position = Vector3.MoveTowards(
+        transform.position,
+        _currentPoint.position,
+        Time.deltaTime * Speed);
+        
+        if (transform.position == _currentPoint.position)
+        {
+            _index++;
+            MyAnimator.Play("Walk");
+            if (_index >= Points.Length)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _currentPoint = Points[_index];
+            }
+
+        }
+
+    }
+}
---
+++ TowerDefense/Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Enemy EnemyPrefab;
+    public float SpawnDeley;
+    public Transform[] Points;
+
+    private float _timer;
+    void Start()
+    {
+        _timer = SpawnDeley;
+    }
+
+   
+    void Update()
+    {
+        _timer -= Time.deltaTime;
+        if(_timer <= 0){
+            Enemy enemy = Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
+            enemy.Points = Points;
+
+            _timer = SpawnDeley;
+        }
+    }
+}
